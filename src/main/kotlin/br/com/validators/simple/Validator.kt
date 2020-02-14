4b114008e36fb642ba7f2d50a1802f66513b3119
Joao Fernandes Lima <joaolima@deliz.com.br>
2020-02-14 17:03:01 -0300

    add metodos "inlined" para exibir as mensagens via lambdas

--- src/main/kotlin/br/com/validators/simple/Validator.kt
+++ src/main/kotlin/br/com/validators/simple/Validator.kt
@@ -2,8 +2,10 @@ package br.com.validators.simple
 
 import br.com.validators.simple.enums.Severity
 import br.com.validators.simple.message.Message
+import javafx.scene.control.Alert
 import java.util.*
 import java.util.function.Consumer
+import kotlin.collections.ArrayList
 
 open class Validator {
 
@@ -97,6 +99,85 @@ open class Validator {
     fun ensure(expression: Boolean?, message: Message): Validator {
         return check(!expression!!, message)
     }
+
+    fun showErrors(block: (erros: List<String>) -> Unit) {
+        block(errors)
+    }
+
+    fun showInformations(block: (informations: List<String>) -> Unit) {
+        block(informations)
+    }
+
+    fun showWarnings(block: (warnings: List<String>) -> Unit) {
+        block(warnings)
+    }
+
+//    private fun showAlert(message: String, type: Alert.AlertType) {
+//        val alert = Alert(type)
+//        alert.title = "SysDeliz"
+//        alert.headerText = null
+//        alert.contentText = message
+//        alert.dialogPane.style =
+//            " -fx-max-width:" + width + "px; -fx-max-height: " + height + "px; -fx-pref-width: " + width + "px; -fx-pref-height: " + height + "px;"
+//        alert.showAndWait()
+//    }
+//
+//    // TODO REMOVER?
+//    fun showAlertErrors() {
+//        defHeightAndWidth()
+//        val exibir = StringBuilder()
+//        var count = 0
+//
+//        for (msg in errors) {
+//            exibir
+//                .append(msg)
+//                .append("\n")
+//            count++
+//        }
+//
+//        if (count > 1) {
+//            height += 5 * count
+//            width += 10 * count
+//        }
+//        showAlert(exibir.toString(), AlertType.ERROR)
+//    }
+//
+//    fun showAlertWarn() {
+//        defHeightAndWidth()
+//
+//        val exibir = StringBuilder()
+//        var count = 0
+//        for (msg in warnings) {
+//            exibir.append(msg).append("\n")
+//            count++
+//        }
+//        if (count > 1) {
+//            height += 5 * count
+//            width += 10 * count
+//        }
+//        showAlert(exibir.toString(), AlertType.WARNING)
+//    }
+//
+//    fun showAlertInfo() {
+//        defHeightAndWidth()
+//
+//        val exibir = StringBuilder()
+//        var count = 0
+//        for (msg in informations) {
+//            exibir.append(msg).append("\n")
+//            count++
+//        }
+//        if (count > 1) {
+//            height += 5 * count
+//            width += 10 * count
+//        }
+//        showAlert(exibir.toString(), AlertType.INFORMATION)
+//    }
+//
+//    private fun defHeightAndWidth(){
+//        height = 200
+//        width = 400
+//    }
 
     init {
         messages = ArrayList()
